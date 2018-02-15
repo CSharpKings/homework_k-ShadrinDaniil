8bb99d41df4adfc31a481b51ba9cf16562e94de3
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-02-15 16:23:18 +0300

    Сумма до 100

---
+++ 10/Work2/Work2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Work2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            while (true)
+            {
+                Console.WriteLine("Введи число");
+                int number = int.Parse(Console.ReadLine());
+                sum = sum + number;
+                if (sum <= 100)
+                {
+
+                    Console.WriteLine(sum);
+                }
+                if ( sum > 100)
+                {
+                   Console.ReadLine();
+                    break;
+
+                }
+            }
+
+        }
+    }
+}
