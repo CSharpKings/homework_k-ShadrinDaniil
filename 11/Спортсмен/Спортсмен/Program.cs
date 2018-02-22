48d4afcd58bc42315b08c2a17ee0b41ce47cc874
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-02-22 16:47:31 +0300

    Задача 1 из 11

---
+++ 11/Спортсмен/Спортсмен/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Спортсмен
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            double sum = 0;
+            double y = 30;
+            double x = 10;
+
+            double quantity = 0;
+            while (true)
+            {
+                quantity = quantity + 1;
+                double x2 = x / 100 * 10;    
+                sum = x + sum;
+                x = x + x2;
+                if (y <= sum)
+                {
+                    Console.WriteLine(quantity);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
