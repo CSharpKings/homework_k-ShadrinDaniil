566b3352059f81e7f086efe58bd5d6b6474f5f14
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-03-01 18:12:51 +0300

    Домашка на 12

---
+++ 12/Среднее арифметическое/Среднее арифметическое/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random gen = new Random();
+            
+            int sum = 0;
+            for (int num = 0; num <= 4; num++)
+            {
+                int number = gen.Next(5);
+                sum = sum + number;
+                
+                 Console.WriteLine(number);
+                    
+            }
+                    Console.WriteLine("Среднее число");
+                    Console.WriteLine(sum / 5);
+                
+                
+            
+                    Console.ReadLine();
+        }
+    }
+}
---
+++ 12/Число кратное семи/Число кратное семи/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 100; number <= 200; number++)
+            {
+                if ( number % 7 == 0  )
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
