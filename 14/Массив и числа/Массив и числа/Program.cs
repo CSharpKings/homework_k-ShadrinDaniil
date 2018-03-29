e16421f9cf3cad86b12f27a60aaf97288878d55e
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-03-29 17:59:58 +0300

    Массив и числа

---
+++ 14/Массив и числа/Массив и числа/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {0, -8, 6, -15, 2};
+           
+            Console.WriteLine("напиши номер");
+            int number = int.Parse(Console.ReadLine());
+            if (numbers[number] < 0)
+            {
+                Console.WriteLine("-");
+            }
+            else
+            {
+                Console.WriteLine("+");
+            }
+            Console.ReadLine();
+
+
+        } 
+    }
+}
