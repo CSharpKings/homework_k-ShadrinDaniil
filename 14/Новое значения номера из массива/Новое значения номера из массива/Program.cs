a69d48f15ba7a0f65441f5e20ab7e223db74fb18
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-03-29 17:58:40 +0300

    Новое значения номера из массива

---
+++ 14/Новое значения номера из массива/Новое значения номера из массива/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3 };
+            Console.WriteLine("Введи число");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введи новое значение");
+            int newValue = int.Parse(Console.ReadLine());
+            numbers[number] = newValue;
+            for (int i = 0; i <= numbers.Length - 1; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
