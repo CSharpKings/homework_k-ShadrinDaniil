1177f340c78b2264d4d6c95655347d5bebac74ae
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-04-12 18:22:55 +0300

    Больше прошлого числа

---
+++ 15/Больше прошлого числа/Больше прошлого числа/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 46, 0, 8, 3, 56, 6, 1, 7 };
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i - 1]  < numbers[i])
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
