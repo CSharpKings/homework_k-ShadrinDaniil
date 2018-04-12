52f0dce489a0488fd2d429d64c72363e7bfd5ac8
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-04-12 18:23:16 +0300

    Пары знаков

---
+++ 15/Пары знаков/Пары знаков/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 89, 7, -1, -2, -6, 6 };
+                for (int i = 1; i < numbers.Length; i++)
+                {
+                    if(numbers[i - 1] > 0 && numbers[i] > 0||numbers[i - 1] < 0 && numbers[i] < 0)
+                    {
+                     
+                        Console.WriteLine(numbers[i] + " " + numbers[i - 1]);
+                        break;
+                    }
+                    
+                }
+            Console.ReadLine();
+
+
+        }
+    }
+}
