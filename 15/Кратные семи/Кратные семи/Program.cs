fbd571d52b876230ce37a51a8bc256d28a7f9766
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-04-19 17:52:12 +0300

    Кратные семи

---
+++ 15/Кратные семи/Кратные семи/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[17];
+            int number = 60;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = number;
+                
+                number  = number - 3;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+
+}
