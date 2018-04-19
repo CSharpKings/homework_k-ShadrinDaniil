681d1d790572060980a3207a323f35cff98538d6
ShadrinDaniil <34743608+ShadrinDaniil@users.noreply.github.com>
2018-04-19 17:52:40 +0300

    Рост людей

---
+++ 15/Рост людей/Рост людей/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] people = new int[12];
+            Random rand = new Random();
+            
+            for (int i = 0; i < people.Length; i++)
+            {
+                int sm = rand.Next(163, 191);
+                people[i] = sm;
+            }
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.WriteLine(people[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
